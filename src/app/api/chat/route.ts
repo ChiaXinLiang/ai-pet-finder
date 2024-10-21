73249cd152916709eb4ef419f54413d6d8b6c7cb
Jack Herrington <jherr@pobox.com>
2024-10-20 17:02:40 -0700

    first commit

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,5 +1,23 @@
 import { openai } from "@ai-sdk/openai";
-import { convertToCoreMessages, streamText } from "ai";
+import { convertToCoreMessages, streamText, tool } from "ai";
+import { z } from "zod";
+
+import { pets } from "@/pets";
+
+const sizes = {
+  small: {
+    cat: 5,
+    dog: 25,
+  },
+  medium: {
+    cat: 7,
+    dog: 50,
+  },
+  large: {
+    cat: 99,
+    dog: 99,
+  },
+};
 
 export const maxDuration = 30;
 
@@ -9,8 +27,42 @@ export async function POST(req: Request) {
   const result = await streamText({
     model: openai("gpt-4o"),
     system: `You are a helpful assistant.
+You should display a recommended pet or pets after with the showPets tool.
 Your goal is to answer questions about adoptable pets.`,
     messages: convertToCoreMessages(messages),
+    maxSteps: 4,
+    tools: {
+      showPets: tool({
+        description: `show recommended pets after using the getPets tool.`,
+        parameters: z.object({
+          pets: z.array(
+            z.object({
+              id: z.string().describe("the id of the pet to show"),
+              recommendation: z
+                .string()
+                .describe("a recommendation for the pet"),
+            })
+          ),
+        }),
+      }),
+      getPets: tool({
+        description: `get a list of available pets to answer questions about adoption.`,
+        parameters: z.object({
+          type: z.enum(["cat", "dog"]),
+          size: z.enum(["small", "medium", "large"]),
+        }),
+        execute: async ({
+          type,
+          size,
+        }: {
+          type: "cat" | "dog";
+          size: "small" | "medium" | "large";
+        }) =>
+          pets
+            .filter((pet) => pet.type === type)
+            .filter((pet) => pet.weight <= sizes[size][type]),
+      }),
+    },
   });
 
   return result.toDataStreamResponse();
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,9 +5,18 @@ import Markdown from "react-markdown";
 
 import { Input } from "@/components/ui/input";
 
+import type { Pet } from "@/pets";
+import PetCard from "@/pet-card";
+
 export default function Chat() {
   const { messages, input, handleInputChange, handleSubmit, isLoading } =
-    useChat();
+    useChat({
+      async onToolCall({ toolCall }) {
+        if (toolCall.toolName === "showPets") {
+          return "Pets were shown to the user.";
+        }
+      },
+    });
 
   return (
     <div className="flex flex-col w-full max-w-4xl pb-24 pt-4 mx-auto">
@@ -18,7 +27,34 @@ export default function Chat() {
         {messages.map((message) => (
           <div key={message.id} className="flex flex-row gap-2">
             <div className={message.role === "user" ? "font-bold" : ""}>
-              <Markdown>{message.content}</Markdown>
+              {message.toolInvocations ? (
+                message.toolInvocations.map((tool, index) => (
+                  <>
+                    {tool.toolName === "showPets" &&
+                      tool.state === "result" && (
+                        <div className="flex flex-wrap">
+                          {tool.args.pets.map(
+                            (
+                              pet: Pet & { recommendation: string },
+                              petIndex: number
+                            ) => (
+                              <div
+                                className="p-2 w-1/2"
+                                key={`${message.id}-${index}-${petIndex}`}
+                              >
+                                <PetCard {...pet} />
+                              </div>
+                            )
+                          )}
+                        </div>
+                      )}
+                  </>
+                ))
+              ) : (
+                <div className={message.role === "user" ? "font-bold" : ""}>
+                  <Markdown>{message.content}</Markdown>
+                </div>
+              )}
             </div>
           </div>
         ))}
